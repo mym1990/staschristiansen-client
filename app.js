7f8dbc0cdfcdaccd8afa78b51ba1d2702441383f
Stas Christiansen <schristiansen1@student.gsu.edu>
2021-01-18 16:05:17 -0500

    flexbox stuff

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var text = ["travel", "travel", "travel", "travel", "travel", "travel", "travel", "travel", "travel", "home", "home", "none", "none"];
-for(var i = 0; i <= text.length; i += 1) {
+for(var i = 0; i <= 365; i += 1) {
     var div = document.createElement("div");
-    div.className = "box";
-    if(text[i] == "travel") {
+    div.className = "flex-item";
+    if(i <= text.length && text[i] == "travel") {
         div.style.backgroundColor = '#6930c3';
-    } else if(text[i] == "home") {
+    } else if(i <= text.length &&  text[i] == "home") {
         div.style.backgroundColor = '#80ffdb';
     } else {
         div.style.backgroundColor = '#fff3e6';
